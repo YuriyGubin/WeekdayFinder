5ea1d34b298dc63764164b9caceda16e83f086a7
YuriyGubin <iosdevelop.2022@gmail.com>
2022-09-28 23:03:37 +0500

    Add extension for alert

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -43,3 +43,12 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
